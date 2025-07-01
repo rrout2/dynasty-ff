bdf7f4e9db5f5aee66e643943254824b84fad607
Rishav Rout <rout.rishav@gmail.com>
2025-07-01 14:15:40 -0700

    dry run 2 folder

--- scripts/infinite_bp/monthly_image_sender.py
+++ scripts/infinite_bp/monthly_image_sender.py
@@ -255,7 +255,7 @@ def main():
     try:
         # Authenticate
         uploader.authenticate()
-        folder_id = '1zN44XZVSRTWsP6ahNd3yEO0Q4z-PIDqB' #uploader.create_or_get_folder(args.folder_name)
+        folder_id = '1oILkHHNfKRFYR-KJWzSSjPftSiw1puHW' #uploader.create_or_get_folder(args.folder_name)
         print(f"Folder link: https://drive.google.com/drive/folders/{folder_id}")
         for i in range(len(sender.league_id_list)):
             if sender.league_id_list[i] == '' or sender.league_id_list[i] == None:
