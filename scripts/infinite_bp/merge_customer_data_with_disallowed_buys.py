fa37a64cdbd22dbb0492af4db477981802944461
Rishav Rout <rout.rishav@gmail.com>
2025-08-07 14:20:52 -0700

    refactor

--- scripts/infinite_bp/merge_customer_data_with_disallowed_buys.py
+++ scripts/infinite_bp/merge_customer_data_with_disallowed_buys.py
@@ -2,14 +2,14 @@ import json
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-month = 'july2025'
+last_month = 'july2025'
 email_to_buys_path_list = ['email_to_buys.json', 'email_to_buys 2.json', 'email_to_buys 3.json']
 league_id_to_buys_path_list = ['league_id_to_buys.json', 'league_id_to_buys 2.json', 'league_id_to_buys 3.json']
 user_id_to_buys_path_list = ['user_id_to_buys.json', 'user_id_to_buys 2.json', 'user_id_to_buys 3.json']
 
 all_email_to_buys = {}
 for path in email_to_buys_path_list:
-    with open(os.path.join(script_dir, 'email_to_buys', month, path), 'r') as file:
+    with open(os.path.join(script_dir, 'previous_buys', last_month, path), 'r') as file:
         all_email_to_buys = all_email_to_buys | json.load(file)
 
 # with open(os.path.join(script_dir, 'email_to_buys', month, 'email_to_buys.json'), 'r') as file:
@@ -23,7 +23,7 @@ for path in email_to_buys_path_list:
 
 all_league_id_to_buys = {}
 for path in league_id_to_buys_path_list:
-    with open(os.path.join(script_dir, 'email_to_buys', month, path), 'r') as file:
+    with open(os.path.join(script_dir, 'previous_buys', last_month, path), 'r') as file:
         all_league_id_to_buys = all_league_id_to_buys | json.load(file)
 
 # with open(os.path.join(script_dir, 'email_to_buys', month, 'league_id_to_buys.json'), 'r') as file:
@@ -37,7 +37,7 @@ for path in league_id_to_buys_path_list:
 
 all_user_id_to_buys = {}
 for path in user_id_to_buys_path_list:
-    with open(os.path.join(script_dir, 'email_to_buys', month, path), 'r') as file:
+    with open(os.path.join(script_dir, 'previous_buys', last_month, path), 'r') as file:
         all_user_id_to_buys = all_user_id_to_buys | json.load(file)
 
 # with open(os.path.join(script_dir, 'email_to_buys', month, 'user_id_to_buys.json'), 'r') as file:
@@ -67,5 +67,5 @@ for customer in customer_info:
         if user_id in all_user_id_to_buys:
             customer['disallowed'] = all_user_id_to_buys[user_id].split(',')
 
-with open(os.path.join(script_dir, 'email_to_buys', month, f'{month}_customer_info_disallowed_userids.json'), 'w') as file:
+with open(os.path.join(script_dir, 'previous_buys', last_month, f'{last_month}_customer_info_disallowed_userids.json'), 'w') as file:
     json.dump(customer_info, file, indent=4)
