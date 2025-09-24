d0b35eb58ade8cd85637039501e212deef2816f2
Rishav Rout <rout.rishav@gmail.com>
2025-09-24 11:50:09 -0700

    new folder id

--- scripts/infinite_bp/monthly_image_sender.py
+++ scripts/infinite_bp/monthly_image_sender.py
@@ -299,7 +299,7 @@ def main():
     try:
         # Authenticate
         uploader.authenticate()
-        folder_id = '1vocm6w9tnU6ebDjPWOM1x0vdSB4uIHCo' #uploader.create_or_get_folder(args.folder_name)
+        folder_id = '1DeG5mFd0I4yCrA36D0Jj8syggUVMzzpq' #uploader.create_or_get_folder(args.folder_name)
         print(f"Folder link: https://drive.google.com/drive/folders/{folder_id}")
 
         print(f"Running manual URL list ({len(sender.manual_url_list)})...")
