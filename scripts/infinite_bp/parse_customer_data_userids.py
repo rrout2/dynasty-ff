d8128a8323b9d0722d2323583dad461fc868d390
Rishav Rout <rout.rishav@gmail.com>
2025-10-08 18:26:15 -0700

    fix rankings week 6

--- scripts/infinite_bp/parse_customer_data_userids.py
+++ scripts/infinite_bp/parse_customer_data_userids.py
@@ -5,7 +5,7 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the JSON file name
-json_file_name = 'domain_customer_info_weekly5_userids.json'
+json_file_name = 'domain_customer_info_weekly6_userids.json'
 
 last_month = 'july2025'
 
